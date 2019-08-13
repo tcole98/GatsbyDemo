c2ea6ead2079a4381318fac9402368602b422185
tcole98 <tristanhcole@gmail.com>
2019-08-13 22:51:24 +1000

    part 8

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 import { useStaticQuery, Link, graphql } from "gatsby"
 import { css } from "@emotion/core"
 
@@ -17,34 +18,41 @@ export default ({ children }) => {
     `
   )
   return (
-    <div
-      css={css`
-      margin: 0 auto;
-      max-width: 700px;
-      padding: ${rhythm(2)};
-      padding-top: ${rhythm(1.5)};
-    `}
-    >
-      <Link to={`/`}>
-        <h3
-          css={css`
-          margin-bottom: ${rhythm(2)};
-          display: inline-block;
-          font-style: normal;
-        `}
-        >
-          {data.site.siteMetadata.title}
-        </h3>
-      </Link>
-      <Link
-        to={`/about/`}
+    <div className="application">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>My Title</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+      <div
         css={css`
-        float: right;
+        margin: 0 auto;
+        max-width: 700px;
+        padding: ${rhythm(2)};
+        padding-top: ${rhythm(1.5)};
       `}
       >
-        About
-      </Link>
-      {children}
+        <Link to={`/`}>
+          <h3
+            css={css`
+            margin-bottom: ${rhythm(2)};
+            display: inline-block;
+            font-style: normal;
+          `}
+          >
+            {data.site.siteMetadata.title}
+          </h3>
+        </Link>
+        <Link
+          to={`/about/`}
+          css={css`
+          float: right;
+        `}
+        >
+          About
+        </Link>
+        {children}
+      </div>
     </div>
   )
 }
