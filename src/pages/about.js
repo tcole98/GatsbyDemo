52a0844923c84981d59245597dbf3042a1429515
tcole98 <tristanhcole@gmail.com>
2019-08-13 22:10:07 +1000

    part 4

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,21 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Header from "../components/header"
 import Layout from "../components/layout"
 
-export default () => (
+export default ({ data }) => (
   <Layout>
-    <Header headerText="About Gatsby" />
+    <Header headerText={data.site.siteMetadata.title} />
     <Header headerText="It's pretty cool" />
     <p>Such wow. Very React.</p>
   </Layout>
 )
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
